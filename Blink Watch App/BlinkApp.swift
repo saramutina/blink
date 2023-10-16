80cbbd4ea2fdbad8ba1e415c46fffd1598567ef0
Katie Saramutina <saramutina@gmail.com>
2023-10-16 14:17:26 +0400

    Initial Commit

---
+++ Blink Watch App/BlinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlinkApp.swift
+//  Blink Watch App
+//
+//  Created by Katie Saramutina on 16.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Blink_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
